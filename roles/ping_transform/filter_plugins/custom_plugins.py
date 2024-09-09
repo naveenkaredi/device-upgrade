23ec5e6f062d0a26e75c168c9c310f75a4efe230
Navneet Singh <singhnavneet@salesforce.com>
2024-09-09 11:22:24 +0530

    testing filter plugins

--- roles/ping_transform/filter_plugins/custom_plugins.py
+++ roles/ping_transform/filter_plugins/custom_plugins.py
@@ -5,23 +5,11 @@ class FilterModule(object):
         }
 
     def transform(self, data):
-        res = []
-        idx_mappings = {}
-        return { 
-            'd1': {'f': 'd'}, 
-            'd2': {'f': 'h'}
-        }
-       
-        # for idx in range(0, len(data)):
-        #     item = data[idx]
-        #     if item['destination'] not in idx_mappings:      
-        #         current = {}
-        #         current['device'] = item['destination']
-        #         current['ping_values'] = []   
-        #         current['ping_values'].append({item['source']: item['value']})
-        #         idx_mappings[item['destination']] = idx
-        #         res.append(current)
-        #     else:
-        #         lst_idx = idx_mappings[item['destination']]
+        res = {}
+        for item in data:
+            if item['destination'] not in res:
+                res[item['destination']] = {}
+            res[item['destination']][item['source']] = item['value']
+        return res
                 
                            
