31739424f16b07c80c9883d790d06ccf2adf1769
Navneet Singh <singhnavneet@salesforce.com>
2024-09-09 11:00:07 +0530

    testing filter plugins

---
+++ roles/ping_transform/filter_plugins/custom_plugins.py
@@ -0,0 +1,24 @@
+class FilterModule(object):
+    def filters(self):
+        return {
+            'transform': self.transform,
+        }
+
+    def transform(self, data):
+        res = []
+        idx_mappings = {}
+        return { 'd1': {'f', 'd'}, 'd2': {'f', 'h'}}
+       
+        # for idx in range(0, len(data)):
+        #     item = data[idx]
+        #     if item['destination'] not in idx_mappings:      
+        #         current = {}
+        #         current['device'] = item['destination']
+        #         current['ping_values'] = []   
+        #         current['ping_values'].append({item['source']: item['value']})
+        #         idx_mappings[item['destination']] = idx
+        #         res.append(current)
+        #     else:
+        #         lst_idx = idx_mappings[item['destination']]
+                
+                           
--- roles/resolve_rsync_server/filter_plugins/custom_plugins.py
+++
@@ -1,8 +0,0 @@
-class FilterModule(object):
-    def filters(self):
-        return {
-            'transform': self.transform,
-        }
-
-    def transform(self, data):
-        return ["test"]
