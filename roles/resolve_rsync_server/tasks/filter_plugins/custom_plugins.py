b0039c67f515d2a1ec92973d906d7994d18973f5
Navneet Singh <singhnavneet@salesforce.com>
2024-09-09 10:30:51 +0530

    testing filter plugins

---
+++ roles/resolve_rsync_server/tasks/filter_plugins/custom_plugins.py
@@ -0,0 +1,10 @@
+class FilterModule(object):
+    def filters(self):
+        return {
+            'transform': self.transform,
+        }
+
+    def transform(self, data):
+        return {
+            "test": "test"
+        }
